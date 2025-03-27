18a0dfe0dad7032ddc24956135c999c31047b8bc
Matt-practices <kolikoasdpvp@gmail.com>
2025-03-27 23:20:28 +0100

    added code

---
+++ recustion palindrome check.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    public static bool IsPalindrome(string input)
+    {
+        if(input.Length ==0 || input.Length == 1)
+        {
+            return true;
+        }
+
+        if (input[0] == input[input.Length - 1])
+        {
+            return IsPalindrome(input.Substring(1, input.Length - 2));
+        }
+        return false;
+    }
+
+    static void Main()
+    {
+        bool check = IsPalindrome("racecar");
+        Console.WriteLine(check);
+        Console.ReadLine();
+    }
+}
---
+++ tobinary recursion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string binary = ToBinary(233, "");
+    }
+
+
+    public static string ToBinary(int decimalnum, string result)
+    {
+        if (decimalnum == 0) { return result; }
+
+        result = decimalnum % 2 + result;
+        return ToBinary(decimalnum / 2, result);
+    }
+
+}
